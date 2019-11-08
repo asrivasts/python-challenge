c8d643466c3732f8d6edf6d273d2702bcb27c2f1
Anu Srivastava <asrivasts@gmail.com>
2019-11-08 12:55:19 -0500

    Final Homework Submission 11/8

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,11 @@
+#Declare Dependencies
 import os
 import csv
 
-
+#Define input file path
 PyBank_csv = os.path.join("Resources", "budget_data.csv")
 
+#initialize variables
 months = 0
 tot = 0
 avg = 0
@@ -15,6 +17,7 @@ first = 0
 last = 0
 tFirst =0
 
+#open File
 with open(PyBank_csv, newline='') as csvfile:
     
     # CSV reader specifies delimiter and variable that holds contents
@@ -26,20 +29,31 @@ with open(PyBank_csv, newline='') as csvfile:
     # Read each row of data after the header
     for row in csvreader:
         
+        #Stores the current month's Profit/Loss
         curP = float(row[1])
         
 
+        #if this is the first run of the loop, then store the company's starting profit 
         if (tFirst == 0):
+            #store the first value for our final calculations
             first = curP
+            #store last to track the previously read values
             last = curP
+            #change tFirst to 1 to not enter this section of the IF statement
             tFirst = 1
+            #Since this is the first run, we have no previous data.  So the assumption is there is no change
             change = 0
         else:
+            #if its not the first run, calculate the change in profit
             change = curP-last
+            #store the current value into the previous value variable
             last = curP
-        
+        #increment the month counter
         months = months + 1
+        #increment the total profit over the full period
         tot = tot + curP
+
+        #if the change in profit is higher than the highest change or lower than the lowest change thus far, store this current change in the appropriate variable
         if (change > maxP):
             maxP = change
             monthP = row[0]
